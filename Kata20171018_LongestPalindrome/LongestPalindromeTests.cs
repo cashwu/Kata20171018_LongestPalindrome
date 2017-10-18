71173a3ab270bee66cae46425a1f4a08a562fe40
cash <cash@cashwu.com>
2017-10-18 21:34:34 +0800

    input null string

--- Kata20171018_LongestPalindrome/LongestPalindromeTests.cs
+++ Kata20171018_LongestPalindrome/LongestPalindromeTests.cs
@@ -7,8 +7,19 @@ namespace Kata20171018_LongestPalindrome
     public class LongestPalindromeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_null_string()
         {
+            var kata = new Kata();
+            var result = kata.GetLongestPalindrome(null);
+            Assert.AreEqual(0, result);
+        }
+    }
+
+    public class Kata
+    {
+        public int GetLongestPalindrome(string s)
+        {
+            return 0;
         }
     }
 }
